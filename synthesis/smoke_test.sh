8103079f6b4a2f758d01a0c119e04237d88454a4
yinzhihan <yinzhihan@bytedance.com>
2026-05-27 08:44:55 +0000

    0527:tmp

--- synthesis/smoke_test.sh
+++ synthesis/smoke_test.sh
@@ -1,7 +1,7 @@
 # Graph Construction without image fetch
 python run_min_graph.py \
   --seed-url https://en.wikipedia.org/wiki/Kobe_Bryant \
-  --store-dir runs/kobe_text_only_20_100_depth_6 \
+  --store-dir runs/kobe_text_only_20_100_depth_7 \
   --reader-base-url http://10.124.138.16:8004 \
   --fresh \
   --no-images \
@@ -9,9 +9,8 @@ python run_min_graph.py \
   --max-steps 64 \
   --max-nodes 128 \
   --max-depth 64 \
-  --parallel-workers 16 \
-  --batch-size 16
-
+  --parallel-workers 32 \
+  --batch-size 32
 # Graph Construction with image fetch
 # python run_min_graph.py \
 #   --seed-url https://en.wikipedia.org/wiki/Kobe_Bryant \
