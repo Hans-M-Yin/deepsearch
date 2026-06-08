6a1705956a9823635da3cee414812d10103e5980
yinzhihan <yinzhihan@bytedance.com>
2026-06-08 08:17:33 +0000

    0608:tmp

--- synthesis/smoke_test_visual.sh
+++ synthesis/smoke_test_visual.sh
@@ -1,8 +1,8 @@
-mkdir -p runs/multi_seed_visual_smoke_3
+mkdir -p runs/multi_seed_visual_smoke_4
 echo "Start Generate."
 python synthesis/run_min_graph.py \
   --seed-urls-file synthesis/seeds.txt \
-  --store-dir runs/multi_seed_visual_smoke_3 \
+  --store-dir runs/multi_seed_visual_smoke_4 \
   --reader-base-url http://10.124.136.6:8004 \
   --fresh \
   --skip-attributes \
