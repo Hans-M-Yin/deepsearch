5485c284ba6f15790527596feb4053f8b245ab2a
Hans-M-Yin <3200636808@qq.com>
2026-06-17 09:14:35 +0800

    0617:tmp

--- synthesis/smoke_visual.sh
+++ synthesis/smoke_visual.sh
@@ -5,7 +5,6 @@ python synthesis/run_min_graph.py \
   --seed-urls-file synthesis/seeds.txt \
   --store-dir runs/multi_seed_visual_8192_1 \
   --reader-base-url http://10.124.136.252:8004 \
-  --fresh \
   --skip-attributes \
   --max-neighbors 4 \
   --max-steps 12345678 \
