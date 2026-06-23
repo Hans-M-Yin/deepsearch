5934428f1e8e3344a77be9bb21d64639b696489b
Hans-M-Yin <3200636808@qq.com>
2026-06-23 17:38:00 +0800

    0623:fix reader adress

--- synthesis/smoke_visual.sh
+++ synthesis/smoke_visual.sh
@@ -4,7 +4,7 @@ source synthesis/.env
 python synthesis/run_min_graph.py \
   --seed-urls-file synthesis/seeds.txt \
   --store-dir runs/multi_seed_visual_8192_1 \
-  --reader-base-url http://10.124.137.241:8004 \
+  --reader-base-url http://10.124.138.7:8004 \
   --skip-attributes \
   --max-neighbors 4 \
   --max-steps 12345678 \
