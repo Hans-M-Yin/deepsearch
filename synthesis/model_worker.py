7d6f0edc7c12d288a67462c1b914b24e517a5912
yinzhihan <yinzhihan@bytedance.com>
2026-06-29 03:59:44 +0000

    0629:tmp

--- synthesis/model_worker.py
+++ synthesis/model_worker.py
@@ -566,6 +566,7 @@ class ModelRouterWorkerClient:
                 return client.generate(request)
             except Exception as exc:
                 last_error = exc
+                print(request)
                 if attempt_index >= 1:
                     break
                 print(
