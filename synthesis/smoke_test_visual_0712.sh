46a640890c0334ee00789d8c52dbbe67c4524b0f
Hans-M-Yin <3200636808@qq.com>
2026-07-15 07:58:11 +0800

    0715:tmp

--- synthesis/smoke_test_visual_0712.sh
+++ synthesis/smoke_test_visual_0712.sh
@@ -1,15 +1,15 @@
-mkdir -p runs/0712_multi_seed_visual_test8_8192
+mkdir -p runs/0712_multi_seed_visual_test_8192_2
 echo "Start Generate."
 source synthesis/.env
 python synthesis/run_min_graph.py \
   --seed-urls-file synthesis/seeds.txt \
-  --store-dir runs/0712_multi_seed_visual_test_8192\
+  --store-dir runs/0712_multi_seed_visual_test_8192_2\
   --reader-base-url http://localhost:8004 \
   --skip-attributes \
   --max-neighbors 4 \
   --max-steps 12345678 \
   --max-nodes 8192 \
   --max-depth 128 \
-  --parallel-workers 128 \
-  --batch-size 128 \
+  --parallel-workers 90 \
+  --batch-size 90 \
   --image-backend serper 
